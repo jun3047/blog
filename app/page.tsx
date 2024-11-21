c8643e6aace99d9df648107cd7ba71d8d020c241
jun <junjung3047@naver.com>
2024-11-21 20:38:19 +0900

    메타 데이터 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
 export default function Home() {
   return <h1>Jun&apos;s Log</h1>;
 }
+
+export const metadata: Metadata = {
+  title: "준의 블로그",
+  description: "정준의 블로그임",
+};
