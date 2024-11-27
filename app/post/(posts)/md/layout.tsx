b61cb7a3e82f4f93cc6fa8e1548b983e657b52a0
jun3047 <jeongjun3047@gmail.com>
2024-11-27 16:59:05 +0900

    post 크기 맞추기

--- app/post/(posts)/md/layout.tsx
+++ app/post/(posts)/md/layout.tsx
@@ -4,10 +4,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="md:flex justify-center">
-        <div className="mt-24 md:m-20 md:w-full w-[768px]">
-          <div className="prose prose-invert md:w-full w-[768px]">
-            {children}
-          </div>
+        <div className="mt-20 w-full px-10 md:mt-24 md:p-0 md:w-[768px]">
+          <div className="prose prose-invert">{children}</div>
         </div>
       </div>
       <Nav />
