191ffe9c5feaa5123122ca30b9a0a91bcfdd8cb9
jun <junjung3047@naver.com>
2024-11-29 21:21:00 +0900

    tag 구분해서 메인에 뿌려주기

--- lib/utils/post.ts
+++ lib/utils/post.ts
@@ -1,11 +1,27 @@
 import { Post, Tag } from "@/@type/post";
 
-type PostsByTag = Partial<Record<Tag, Post[]>>;
+const etcTag = "작성 중인";
 
-export const groupPostsByTag = (posts: Post[]): PostsByTag => {
+type PostsByTag = Partial<Record<Tag, Post[]>> & {
+  [key in typeof etcTag]?: Post[];
+};
+
+export const groupPostsByTag = (
+  posts: Post[],
+  displayTags: Tag[] = ["프론트엔드", "사업", "디자인"]
+): PostsByTag => {
   return posts.reduce((acc: PostsByTag, cur) => {
-    if (!acc[cur.tag]) acc[cur.tag] = [cur];
-    else acc[cur.tag]?.push(cur);
+    const primaryTag = displayTags.find(tag => cur.tags.includes(tag));
+
+    if (primaryTag) {
+      if (!acc[primaryTag]) acc[primaryTag] = [cur];
+      else acc[primaryTag]?.push(cur);
+    } else {
+      // "기타" 그룹에 추가
+      if (!acc[etcTag]) acc[etcTag] = [cur];
+      else acc[etcTag]?.push(cur);
+    }
+
     return acc;
   }, {});
 };
