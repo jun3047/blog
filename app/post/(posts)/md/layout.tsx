86b13231eaf75f7a13a162a0efa44fe7d6e5f036
jun <junjung3047@naver.com>
2024-12-01 17:19:36 +0900

    padding 추가

--- app/post/(posts)/md/layout.tsx
+++ app/post/(posts)/md/layout.tsx
@@ -4,7 +4,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="md:flex md:justify-center">
-        <div className="px-10 mt-20 prose prose-invert w-full">{children}</div>
+        <div className="px-10 pb-32 mt-20 prose prose-invert w-full">
+          {children}
+        </div>
       </div>
       <Nav />
     </>
