feced6d9f99174c433385eb7da8a6ad2a135eea6
jun3047 <jeongjun3047@gmail.com>
2024-12-04 12:58:16 +0900

    curation 만들기

--- lib/utils/post.ts
+++ lib/utils/post.ts
@@ -8,20 +8,27 @@ type PostsByTag = Partial<Record<Tag, Post[]>> & {
 
 export const groupPostsByTag = (
   posts: Post[],
-  displayTags: Tag[] = ["프론트엔드", "사업", "디자인"]
+  displayTags: Tag[] = ["프론트엔드", "학습", "사람", "사업"]
 ): PostsByTag => {
-  return posts.reduce((acc: PostsByTag, cur) => {
+  // 초기 객체를 displayTags 순서대로 설정
+  const initialAcc: PostsByTag = displayTags.reduce(
+    (acc, tag) => ({ ...acc, [tag]: [] }),
+    {}
+  );
+
+  return posts.reduce((acc, cur) => {
     const primaryTag = displayTags.find(tag => cur.tags.includes(tag));
 
     if (primaryTag) {
-      if (!acc[primaryTag]) acc[primaryTag] = [cur];
-      else acc[primaryTag]?.push(cur);
+      acc[primaryTag]?.push(cur);
     } else {
-      // "기타" 그룹에 추가
-      if (!acc[etcTag]) acc[etcTag] = [cur];
-      else acc[etcTag]?.push(cur);
+      if (!acc[etcTag]) {
+        acc[etcTag] = [cur];
+      } else {
+        acc[etcTag]?.push(cur);
+      }
     }
 
     return acc;
-  }, {});
+  }, initialAcc);
 };
